b52d975b996a2547776d80d28afa9b48e0608056
jidnesh madhavi <63727902+Jidnesh16@users.noreply.github.com>
2020-11-01 14:57:56 +0530

    Add files via upload

---
+++ Day5/main.java
@@ -0,0 +1,13 @@
+package quizgame;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        quiz_game game=new quiz_game();
+        game.initGame();
+        game.play();
+
+    }
+
+}
